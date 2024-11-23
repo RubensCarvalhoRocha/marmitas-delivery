9c974572a257d4d454826e78e9d8e90b71b95f5f
RubensCarvalhoRocha <rocharubens4@gmail.com>
2024-11-23 20:53:26 -0300

    feat: rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Routes } from '@angular/router';
-import { SignUpComponent } from './page/sign-up/sign-up.component';
+import { SignUpComponent } from './modules/sign-up/sign-up.component';
 import { AuthGuard } from './guards/auth.guard';
 import { AppLayoutComponent } from './layout/app.layout.component';
-import { SignInComponent } from './page/sign-in/sign-in.component';
-import { PedidosComponent } from './page/pedidos/pedidos.component';
+import { SignInComponent } from './modules/sign-in/sign-in.component';
+import { PedidosComponent } from './modules/pedidos/pedidos.component';
+import { RotasComponent } from './modules/rotas/rotas.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'pedidos', pathMatch: 'full' },
 
   //Rotas não autenticadas
   { path: 'sign-up', component: SignUpComponent },
@@ -17,12 +18,29 @@ export const routes: Routes = [
     path: 'pedidos',
     component: AppLayoutComponent,
     canActivate: [AuthGuard],
+    data: { roles: ['ROLE_ADMIN'] },
 
     children: [
       {
         path: '',
         component: PedidosComponent,
+        canActivate: [AuthGuard],
         data: { roles: ['ROLE_ADMIN'] },
+      },
+    ],
+  },
+  {
+    path: 'rotas',
+    component: AppLayoutComponent,
+    canActivate: [AuthGuard],
+    data: { roles: ['ROLE_ADMIN'] },
+
+    children: [
+      {
+        path: '',
+        component: RotasComponent,
+        canActivate: [AuthGuard],
+        data: { roles: ['ROLE_USER', 'ROLE_ADMIN'] },
       },
     ],
   },
