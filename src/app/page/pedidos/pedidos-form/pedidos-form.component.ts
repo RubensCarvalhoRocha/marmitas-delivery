76a5775f6166b0660d31e5031b48a655eea685c6
RubensCarvalhoRocha <rocharubens4@gmail.com>
2024-11-23 17:17:18 -0300

    feat: pedidos

---
+++ src/app/page/pedidos/pedidos-form/pedidos-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pedidos-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './pedidos-form.component.html',
+  styleUrl: './pedidos-form.component.css'
+})
+export class PedidosFormComponent {
+
+}
