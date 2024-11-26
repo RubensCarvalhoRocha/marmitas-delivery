80221973410a6f0b50a9bbc57658fc537d29abab
RubensCarvalhoRocha <rocharubens4@gmail.com>
2024-11-26 02:50:15 -0300

    feat: formulario pedido wip

--- src/app/modules/pedidos/pedidos-form/pedidos-form.component.ts
+++ src/app/modules/pedidos/pedidos-form/pedidos-form.component.ts
@@ -1,16 +1,93 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { DropdownModule } from 'primeng/dropdown';
+import { Component, OnInit } from '@angular/core';
 import { MapaComponent } from '../../mapa/mapa.component';
+import { RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { CalendarModule } from 'primeng/calendar';
+import { ChipsModule } from 'primeng/chips';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputMaskModule } from 'primeng/inputmask';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { CascadeSelectModule } from 'primeng/cascadeselect';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { InputTextModule } from 'primeng/inputtext';
+import { CommonModule } from '@angular/common';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-pedidos-form',
   standalone: true,
-  imports: [DropdownModule, FormsModule, MapaComponent],
+  imports: [
+    DropdownModule,
+    FormsModule,
+    MapaComponent,
+    ReactiveFormsModule,
+    InputTextModule,
+    ButtonModule,
+    InputTextModule,
+    MultiSelectModule,
+    ButtonModule,
+    CommonModule,
+    AutoCompleteModule,
+    CalendarModule,
+    ChipsModule,
+    InputMaskModule,
+    InputNumberModule,
+    CascadeSelectModule,
+    MultiSelectModule,
+    InputTextareaModule,
+    InputTextModule,
+    RouterModule,
+  ],
   templateUrl: './pedidos-form.component.html',
   styleUrl: './pedidos-form.component.css',
 })
-export class PedidosFormComponent {
+export class PedidosFormComponent implements OnInit {
+  form!: FormGroup;
+  formasPagamento = [
+    { label: 'Dinheiro', value: 'dinheiro' },
+    { label: 'Cartão de Crédito', value: 'credito' },
+    { label: 'Cartão de Débito', value: 'debito' },
+    { label: 'PIX', value: 'pix' },
+  ];
+  marmitas = [
+    { label: 'Marmita Pequena', value: 'pequena' },
+    { label: 'Marmita Média', value: 'media' },
+    { label: 'Marmita Grande', value: 'grande' },
+  ];
+
+  constructor(private _formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.form = this._formBuilder.group({
+      id: [null],
+      nomeCliente: ['', [Validators.required]],
+      cpf: ['', [Validators.required, Validators.pattern(/^\d{11}$/)]],
+      telefone: ['', [Validators.required, Validators.pattern(/^\d{10,11}$/)]],
+      pedido: [[], Validators.required],
+      observacao: [''],
+      formaPagamento: [null, Validators.required],
+      troco: [false],
+      valorTotal: [{ value: '', disabled: true }],
+    });
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      console.log(this.form.value);
+      // Adicione a lógica para salvar o pedido
+    } else {
+      console.error('Formulário inválido');
+    }
+  }
+
   selectedState: any = null;
 
   states: any[] = [
