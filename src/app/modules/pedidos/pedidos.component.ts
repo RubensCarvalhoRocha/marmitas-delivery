c01221756a109d5ae4cf330cc0cb50d082ffdc7b
RubensCarvalhoRocha <rocharubens4@gmail.com>
2024-11-26 22:31:09 -0300

    feat: listagem de pedidos wip

--- src/app/modules/pedidos/pedidos.component.ts
+++ src/app/modules/pedidos/pedidos.component.ts
@@ -2,15 +2,29 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-pedidos',
   standalone: true,
-  imports: [ButtonModule, CommonModule],
+  imports: [ButtonModule, CommonModule, TableModule],
   templateUrl: './pedidos.component.html',
   styleUrl: './pedidos.component.css',
 })
 export class PedidosComponent {
+  products: any[] = [
+    {
+      name: 'Product 1',
+      price: 29.99,
+      image: 'product1.jpg',
+    },
+    {
+      name: 'Product 2',
+      price: 49.99,
+      image: 'product2.jpg',
+    },
+  ];
+
   constructor(private router: Router) {}
 
   navigateTo(id: string): void {
