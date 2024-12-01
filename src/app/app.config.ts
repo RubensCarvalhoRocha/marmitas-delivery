e3d7bdf19f6cdf6f8ea0f3cb23c9f23d732f3d00
RubensCarvalhoRocha <rocharubens4@gmail.com>
2024-12-01 19:19:43 -0300

    feat: add mascara

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,20 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideAnimations } from '@angular/platform-browser/animations'
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './interceptors/auth.interceptor';
+import { NgxMaskConfig, provideEnvironmentNgxMask } from 'ngx-mask';
 
+const maskConfig: Partial<NgxMaskConfig> = {
+  validation: false,
+};
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations(), 
-    provideHttpClient(withInterceptors([authInterceptor]))
-  ]
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    provideEnvironmentNgxMask(maskConfig),
+    provideHttpClient(withInterceptors([authInterceptor])),
+  ],
 };
