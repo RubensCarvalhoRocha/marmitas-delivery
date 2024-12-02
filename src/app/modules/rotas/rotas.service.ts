12df354c620fb080efeaea739c0ae637cb36dc07
RubensCarvalhoRocha <rocharubens4@gmail.com>
2024-12-01 22:04:39 -0300

    feat: service para listagem dos pontos de entrega ordenados

--- src/app/modules/rotas/rotas.service.ts
+++ src/app/modules/rotas/rotas.service.ts
@@ -1,9 +1,45 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Pedido } from '../../model/pedido';
+import { BehaviorSubject, catchError, Observable, tap, throwError } from 'rxjs';
+import { environment } from '../../../environment/environment';
+import { DeliveryPoints } from '../../model/deliveryPoints';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RotasService {
+  // Private BehaviorSubject para armazenar e gerenciar os pedidos
+  private _pontosDeEntrega: BehaviorSubject<DeliveryPoints[]> =
+    new BehaviorSubject<DeliveryPoints[]>([]);
 
-  constructor() { }
+  /**
+   * Constructor
+   */
+  constructor(private _http: HttpClient) {}
+
+  // -----------------------------------------------------------------------------------------------------
+  // @ Acessores
+  // -----------------------------------------------------------------------------------------------------
+  get pontosDeEntrega$(): Observable<DeliveryPoints[]> {
+    return this._pontosDeEntrega.asObservable();
+  }
+
+  // -----------------------------------------------------------------------------------------------------
+  // @ Métodos Públicos
+  // -----------------------------------------------------------------------------------------------------
+
+  listarPontosDeEntrega(): Observable<DeliveryPoints[]> {
+    return this._http
+      .get<DeliveryPoints[]>(`${environment.api}/deliveryPoints/route`)
+      .pipe(
+        tap((pontosDeEntrega: DeliveryPoints[]) => {
+          // Atualiza o BehaviorSubject com os pontosDeEntrega recebidos
+          this._pontosDeEntrega.next(pontosDeEntrega);
+        }),
+        catchError((error) => {
+          console.error('Erro ao buscar pontos de entrega:', error);
+          throw error;
+        })
+      );
+  }
 }
